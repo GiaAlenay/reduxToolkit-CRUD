80e7327442db5639d0eeb61e31609b9b32e35662
Gianella Leon <giigl@hotmail.com>
2023-03-20 21:44:18 -0500

    onchange

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import { TaskForm } from './components/taskForm';
 import { TaskList } from './components/taskList';
 
 function App() {
   return (
     <div className="App">
+      <TaskForm/>
       <TaskList/>
     </div>
   );
--- src/components/taskForm.jsx
+++ src/components/taskForm.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react"
+
 export const  TaskForm=()=>{
+    const [task,setTask]=useState({
+        title:'',
+        description:''
+    })
+
+    const handleChange=(e)=>{
+        setTask({...task,
+        [e.target.name]:e.target.value})
+        console.log(task)
+    }
  return(
     <form>
-        <input type='text' name='title'placeholder="title..."/>
-        <textarea name='descrption'placeholder="description..."/>
+        <input type='text' name='title'placeholder="title..." onChange={handleChange}/>
+        <textarea name='description'placeholder="description..." onChange={handleChange}/>
         <button>save</button>
     </form>
  )
